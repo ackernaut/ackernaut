a24f26eba18297980e04fb804f815e4df63bce78
Terry Acker <ackernaut@gmail.com>
2015-09-11 15:24:40 -0500

    First pass at an initial 2.0.0 landing page

--- page-custom.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Template Name: Custom
-*/
-?>
-
-<?php get_header(); ?>
-Test
-<?php get_footer(); ?>
--- work.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Template Name: Work
-*/
-?>
-
-<?php get_header(); ?>
-<p>Work it word!</p>
-<?php get_footer(); ?>
